9c9b82250aa89d3505dd60ae9764659da193cb96
siimmannik <144935979+siimmannik@users.noreply.github.com>
2026-06-16 08:52:14 +0300

    Fix stretch demo with head-tilt figure

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='taichi-v4';
-const ASSETS=["./", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png", "images/hero.jpg", "images/move-ball.jpg", "images/move-breathe.jpg", "images/move-close.jpg", "images/move-cloud.jpg", "images/move-cloud2.jpg", "images/move-gather.jpg", "images/move-knee.jpg", "images/move-meditate.jpg", "images/move-neck.jpg", "images/move-pat.jpg", "images/move-raise.jpg", "images/move-shift.jpg", "images/move-shift2.jpg", "images/move-stretch.jpg", "images/move-stretch2.jpg", "images/move-warmpalms.jpg", "images/move-warmup.jpg", "images/move-wuji.jpg"];
+const CACHE='taichi-v5';
+const ASSETS=["./", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png", "images/hero.jpg", "images/move-ball.jpg", "images/move-breathe.jpg", "images/move-close.jpg", "images/move-cloud.jpg", "images/move-cloud2.jpg", "images/move-gather.jpg", "images/move-knee.jpg", "images/move-meditate.jpg", "images/move-neck.jpg", "images/move-pat.jpg", "images/move-raise.jpg", "images/move-shift.jpg", "images/move-shift2.jpg", "images/move-stretch.jpg", "images/move-warmpalms.jpg", "images/move-warmup.jpg", "images/move-wuji.jpg"];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()).catch(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
